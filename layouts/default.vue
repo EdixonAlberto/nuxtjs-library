0e70d31e337bd0077efb13ba879b5152287972e2
Edixon Piña <edixonalbertto@gmail.com>
2022-07-30 20:56:19 -0400

    create layout to header, footer and home

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
-    <div>header</div>
+    <header class="h-20 mx-4 static top-0">
+      <div class="h-full flex justify-center content-between">
+        <div class="w-14 h-14 ml-auto border rounded">
+          <img src="" alt="logo avatar" />
+        </div>
+      </div>
+    </header>
     <Nuxt />
-    <div>footer</div>
+    <footer class="w-screen absolute bottom-0">
+      <a href="#">Created by Edixon</a>
+    </footer>
   </div>
 </template>
+
+<style scoped>
+* {
+  outline: 1px solid green !important;
+}
+
+header {
+  background: #fbfbfb;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,46 @@
 <template>
-  <h1>Home</h1>
+  <div class="home flex content-between ">
+    <aside class="w-60 relative top-0 left-0 pt-16 transition delay-500">
+      <h3 class="text-3xl text-gray-100">Library</h3>
+
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </aside>
+
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+aside {
+  height: calc(100vh - 80px);
+  padding: 20px;
+
+  background-image: url('/images/libraries.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: lef;
+  color: #fff;
+
+  z-index: 1;
+}
+
+aside::after {
+  content: '';
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: -1;
+}
+</style>
